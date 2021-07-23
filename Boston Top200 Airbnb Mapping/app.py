5b71aed15ec9d962db5f48b2f4865b58b917bcca
UpdatedUserName <78885790+Pascalduc@users.noreply.github.com>
2021-07-22 21:38:17 -0400

    mapping

--- Boston Top200 Airbnb Mapping/app.py
+++ Boston Top200 Airbnb Mapping/app.py
@@ -1,18 +1,3 @@
-import cgi
-import webapp2
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        INDEX_HTML = open('index.html').read()
-        self.response.out.write(INDEX_HTML)
-
-class PageTwo(webapp2.RequestHandler):
-    def post(self):
-        self.response.write('<html><body>You wrote:<pre>')
-        self.response.write(cgi.escape(self.request.get('content')))
-        self.response.write('</pre></body></html>')
-
-application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/sign', PageTwo),
-], debug=True)
+- url: /
+  static_files: index.html
+  upload: index.html
