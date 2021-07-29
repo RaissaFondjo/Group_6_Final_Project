388518bea191190eefd4326b999bdbffd1da7cda
pascalduc <pascalduc@gmail.com>
2021-07-28 22:33:35 -0400

    app

--- app/static/js/logic.js
+++ app/static/js/logic.js
@@ -3,7 +3,6 @@
 const API_KEY = process.env.API_KEY;
 
 
-
 // Store our API endpoint inside queryUrl
 let neighborhoodLink = "../static/data/Boston_Neighborhoods.geojson";
 let listingsLink = "../static/data/listings.geojson";
